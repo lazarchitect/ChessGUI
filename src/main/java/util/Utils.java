b916b279f6a99cfdd0735532391f1a67a59637a4
Eddie <eddie.lazar@yahoo.com>
2020-10-28 02:01:30 -0400

    ditto

--- src/main/java/util/Utils.java
+++ src/main/java/util/Utils.java
@@ -7,4 +7,9 @@ public class Utils {
     public static boolean isDarkTile(int row, int col){
         return (row%2==0 && col%2==0) || (row%2==1 && col%2==1);
     }
+
+    public static boolean outOfBounds(int row, int col){
+        return row < 0 || row > 7 || col < 0 || col > 7;
+    }
+
 }
