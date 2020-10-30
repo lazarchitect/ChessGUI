201e1c11ceabcd62b5a281242405d4b9a09ac0f6
Eddie <eddie.lazar@yahoo.com>
2020-10-30 14:20:31 -0400

    generic pawn and slider logic, added comments

--- src/main/java/logic/MoveLogic.java
+++ src/main/java/logic/MoveLogic.java
@@ -13,13 +13,18 @@ public class MoveLogic {
     private static final int[][] bishopOffsets = {{1,1},{1,-1},{-1,1},{-1,-1}};
     private static final int[][] rookOffsets = {{0,1},{0,-1},{1,0},{-1,0}};
     private static final int[][] royalOffsets = {{1,1},{1,-1},{-1,1},{-1,-1},{0,1},{0,-1},{1,0},{-1,0}};
+    private static final int whiteDirection = -1;
+    private static final int blackDirection = 1;
 
+    // recursive method to scan a straight line across the board for rooks, bishops, and queens
     private static void scan(CoordinateList coords, Board b, int row, int col, int rowOffset, int colOffset, PieceColor pieceColor) {
 
+        // base case: line has exited the board
         if(outOfBounds(row + rowOffset, col + colOffset) ) {
             return;
         }
 
+        // base case: there's a piece in the way
         if(b.hasPieceAt(row + rowOffset, col + colOffset)){
             if(b.pieceAt(row + rowOffset, col + colOffset).getColor() != pieceColor) {
                 coords.add(row + rowOffset, col + colOffset);
@@ -27,75 +32,40 @@ public class MoveLogic {
             return;
         }
 
+        // empty tile? keep going
         coords.add(row + rowOffset, col + colOffset);
         scan(coords, b, row + rowOffset, col + colOffset, rowOffset, colOffset, pieceColor);
 
-
     }
+
 
     public static CoordinateList validPawnMoves(Board b, int row, int col) {
-        Piece currentPiece = b.pieceAt(row, col);
-        if(currentPiece.getColor() == PieceColor.Black) {
-            return validBlackPawnMoves(b, row, col);
-        }
-        else { // white pawn
-            return validWhitePawnMoves(b, row, col);
-        }
-    }
 
-    private static CoordinateList validBlackPawnMoves(Board b, int row, int col) {
-
-        CoordinateList retval = new CoordinateList();
-
-        if(row == 7) return retval;
-
-        if(!b.hasPieceAt(row + 1, col)){
-            retval.add(row + 1, col);
-        }
-        if(row == 1 && !b.hasPieceAt(row + 1, col) && !b.hasPieceAt(row + 2, col)){
-            retval.add(row + 2, col);
-        }
-        if(col != 7 && b.hasWhitePieceAt(row + 1, col + 1)){
-            retval.add(row + 1, col + 1);
-        }
-        if(col != 0 && b.hasWhitePieceAt(row + 1, col - 1)){
-            retval.add(row + 1, col - 1);
-        }
-        return retval;
-    }
-
-    public static CoordinateList validWhitePawnMoves(Board b, int row, int col) {
-
-        CoordinateList retval = new CoordinateList();
-
-        if(row == 0) return retval;
-
-        if(!b.hasPieceAt(row - 1, col)){
-            retval.add(row - 1, col);
-        }
-        if(row == 6 && !b.hasPieceAt(row - 1, col) && !b.hasPieceAt(row - 2, col)){
-            retval.add(row - 2, col);
-        }
-        if(col != 7 && b.hasBlackPieceAt(row - 1, col + 1)){
-            retval.add(row - 1, col + 1);
-        }
-        if(col != 0 && b.hasBlackPieceAt(row - 1, col - 1)){
-            retval.add(row - 1, col - 1);
-        }
-        return retval;
-    }
-
-
-    public static CoordinateList validBishopMoves(Board b, int row, int col){
         PieceColor pieceColor = b.pieceAt(row, col).getColor();
 
-        CoordinateList validMoves = new CoordinateList();
+        int direction = pieceColor == PieceColor.Black ? blackDirection : whiteDirection;
+        int finalRow = pieceColor == PieceColor.Black ? 7 : 0;
+        int starterRow = pieceColor == PieceColor.Black ? 1 : 6;
 
-        for(int[] bishopOffset: bishopOffsets) {
-            scan(validMoves, b, row, col, bishopOffset[0], bishopOffset[1], pieceColor);
+        CoordinateList retval = new CoordinateList();
+
+        if(row == finalRow) return retval; // will never happen once I add promotion
+
+        if(!b.hasPieceAt(row + direction, col)){
+            retval.add(row + direction, col);
         }
-        return validMoves;
+        if(row == starterRow && !b.hasPieceAt(row + direction, col) && !b.hasPieceAt(row + direction*2, col)){
+            retval.add(row + direction*2, col);
+        }
+        if(col != 7 && b.hasWhitePieceAt(row + direction, col + 1)){
+            retval.add(row + direction, col + 1);
+        }
+        if(col != 0 && b.hasWhitePieceAt(row + direction, col - 1)){
+            retval.add(row + direction, col - 1);
+        }
+        return retval;
     }
+
 
     public static CoordinateList validKnightMoves(Board b, int row, int col){
         CoordinateList retval = new CoordinateList();
@@ -113,18 +83,6 @@ public class MoveLogic {
         return retval;
     }
 
-
-    public static CoordinateList validQueenMoves(Board b, int row, int col){
-        PieceColor pieceColor = b.pieceAt(row, col).getColor();
-
-        CoordinateList validMoves = new CoordinateList();
-
-        for(int[] queenOffset: royalOffsets) {
-            scan(validMoves, b, row, col, queenOffset[0], queenOffset[1], pieceColor);
-        }
-        return validMoves;
-    }
-
     public static CoordinateList validKingMoves(Board b, int row, int col){
         CoordinateList retval = new CoordinateList();
         PieceColor pieceColor = b.pieceAt(row, col).getColor();
@@ -136,23 +94,29 @@ public class MoveLogic {
             if (outOfBounds(destRow, destCol)) continue;
 
             if ((b.tileAt(destRow, destCol).isEmpty() || b.pieceAt(destRow, destCol).getColor() != pieceColor)) {
-                // TODO make sure this dest tile isnt dangerous for our dear leader
+                // TODO make sure this dest tile isn't dangerous for our dear leader
                 retval.add(row + kingOffset[0], col + kingOffset[1]);
             }
         }
-
-
         return retval;
     }
 
-
+    public static CoordinateList validQueenMoves(Board b, int row, int col) {
+        return validSliderMoves(b, row, col, royalOffsets);
+    }
     public static CoordinateList validRookMoves(Board b, int row, int col){
-        PieceColor pieceColor = b.pieceAt(row, col).getColor();
+        return validSliderMoves(b, row, col, rookOffsets);
+    }
+    public static CoordinateList validBishopMoves(Board b, int row, int col){
+        return validSliderMoves(b, row, col, bishopOffsets);
+    }
 
+    public static CoordinateList validSliderMoves(Board b, int row, int col, int[][] typeOffsets){
+        PieceColor pieceColor = b.pieceAt(row, col).getColor();
         CoordinateList validMoves = new CoordinateList();
 
-        for(int[] rookOffset: rookOffsets) {
-            scan(validMoves, b, row, col, rookOffset[0], rookOffset[1], pieceColor);
+        for(int[] xyOffset: typeOffsets) {
+            scan(validMoves, b, row, col, xyOffset[0], xyOffset[1], pieceColor);
         }
         return validMoves;
     }
