9f59622e120e2258cd707a7b35565da4074ed3a5
Eddie Lazar <edward.k.lazar@accenture.com>
2020-11-11 23:40:01 -0500

    listView of Items

--- src/main/java/ui/ListBuilder.java
+++ src/main/java/ui/ListBuilder.java
@@ -1,14 +1,32 @@
 package main.java.ui;
 
 import javafx.scene.Group;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.util.Callback;
+import main.java.logic.Tile;
 
 public class ListBuilder {
 
-    public static void buildList(Group root) {
-        ListView<String> list = new ListView<>();
 
-        list.getItems().add("sdfsdf");
+    private static class ListItem {
+        String opponent;
+        String gamemode;
+
+        public ListItem(String opponent, String gamemode){
+            this.opponent = opponent;
+            this.gamemode = gamemode;
+        }
+
+        public String toString() {
+            return this.gamemode + " vs. " + this.opponent;
+        }
+    }
+
+    public static void buildList(Group root) {
+        ListView<ListItem> list = new ListView<>();
+
+        list.getItems().add(new ListItem("Cory", "Correspondence"));
 
         root.getChildren().add(list);
     }
